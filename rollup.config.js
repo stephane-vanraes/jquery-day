ab7f13e06f05e6d7b334919a632ca242451424a9
Stephane Vanraes <stephane.vanraes@fjordline.com>
2020-03-10 09:19:54 +0100

    Remove livereload from production

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,8 @@ import resolve from "@rollup/plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
+const production = !process.env.ROLLUP_WATCH;
+
 export default {
   input: "./src/index.js",
   output: {
@@ -25,6 +27,6 @@ export default {
       }
     }),
     terser(),
-    livereload()
+    !production && livereload()
   ]
 };
